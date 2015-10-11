217b371a1d633e6a1a5d623fd1b2662b3c654698
kuramu1108 <mac@mbpr.local>
2015-10-11 16:36:53 +1100

    setup for basic functionality

---
+++ ConnectUTS/CreateAccountActivity.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using SQLite;
+namespace ConnectUTS
+{
+	[Activity (Label = "Create Account")]			
+	public class CreateAccountActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.CreateAccount);
+
+			EditText studentIDInput = FindViewById<EditText> (Resource.Id.studentIDInput);
+			EditText passwordInput = FindViewById<EditText> (Resource.Id.passwordInput);
+			EditText rePasswordInput = FindViewById<EditText> (Resource.Id.rePasswordInput);
+			Button registerAccountButton = FindViewById<Button> (Resource.Id.registerAccountButton);
+
+			string studentID = String.Empty;
+			string password = String.Empty;
+			string rePassword = String.Empty;
+
+			string path = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal);
+			var accountDB = new SQLiteConnection (System.IO.Path.Combine(path, "account.db"));
+
+			registerAccountButton.Click += (object sender, EventArgs e) => {
+				studentID = studentIDInput.Text;
+				password = passwordInput.Text;
+				rePassword = rePasswordInput.Text;
+
+				if(String.IsNullOrWhiteSpace(studentID) || String.IsNullOrWhiteSpace(password) || String.IsNullOrWhiteSpace(rePassword))
+				{
+					var notFilledAlert = new AlertDialog.Builder(this);
+					notFilledAlert.SetMessage("Please fill the required field");
+					notFilledAlert.SetNegativeButton("OK", delegate{});
+					notFilledAlert.Show();
+				}
+				else
+				{
+					string message = "";
+					var result = accountDB.Query<Account>("SELECT * FROM Account WHERE StudentID = '" + studentID + "'");
+					if (result.Count != 0)
+					{
+						message = "Student ID already exist";
+					}
+					else
+					{
+						if (!password.Equals(rePassword))
+						{
+							message = "Password mismatch";
+						}
+						else
+						{
+							Account acc = new Account();
+							acc.StudentID = studentID;
+							acc.Password = password;
+							accountDB.Insert(acc);
+							message = "Account created";
+						}
+					}
+					var filledAlert = new AlertDialog.Builder(this);
+					filledAlert.SetMessage(message);
+					filledAlert.SetNegativeButton("OK", delegate{});
+					filledAlert.Show();
+					
+				}
+		
+			};
+
+
+		}
+	}
+}
+
--- ConnectUTS/Resources/Resource.designer.cs
+++ ConnectUTS/Resources/Resource.designer.cs
@@ -60,8 +60,44 @@ namespace ConnectUTS
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000b
+			public const int createAccountButton = 2131034123;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int password = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int passwordInput = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int rePassword = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int rePasswordInput = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int registerAccountButton = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int relativeLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int relativeLayout2 = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int relativeLayout3 = 2131034119;
+			
+			// aapt resource value: 0x7f050002
+			public const int studentID = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int studentIDInput = 2131034115;
+			
+			// aapt resource value: 0x7f05000c
+			public const int testButton = 2131034124;
 			
 			static Id()
 			{
@@ -77,7 +113,10 @@ namespace ConnectUTS
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int CreateAccount = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
