802886561156ef105e46f66dbc492d61ec4c8282
Raymond Tedja <Raymond_Tedja88@hotmail.co.id>
2015-10-13 15:05:00 +1100

    conflict fixed

--- ConnectUTS/Resources/Resource.designer.cs
+++ ConnectUTS/Resources/Resource.designer.cs
@@ -41,11 +41,34 @@ namespace ConnectUTS
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040001
+			public const int darkBlue = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int lightBlue = 2130968576;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
+<<<<<<< HEAD
 			public const int edit_text_style = 2130837504;
+=======
+			public const int gradient_background = 2130837504;
+>>>>>>> origin/master
 			
 			// aapt resource value: 0x7f020001
 			public const int ic_launcher = 2130837505;
@@ -63,44 +86,44 @@ namespace ConnectUTS
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000b
-			public const int createAccountButton = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int createAccountButton = 2131099659;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int linearLayout1 = 2131099648;
 			
-			// aapt resource value: 0x7f050005
-			public const int password = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int password = 2131099653;
 			
-			// aapt resource value: 0x7f050006
-			public const int passwordInput = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int passwordInput = 2131099654;
 			
-			// aapt resource value: 0x7f050008
-			public const int rePassword = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int rePassword = 2131099656;
 			
-			// aapt resource value: 0x7f050009
-			public const int rePasswordInput = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int rePasswordInput = 2131099657;
 			
-			// aapt resource value: 0x7f05000a
-			public const int registerAccountButton = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int registerAccountButton = 2131099658;
 			
-			// aapt resource value: 0x7f050001
-			public const int relativeLayout1 = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int relativeLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f050004
-			public const int relativeLayout2 = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int relativeLayout2 = 2131099652;
 			
-			// aapt resource value: 0x7f050007
-			public const int relativeLayout3 = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int relativeLayout3 = 2131099655;
 			
-			// aapt resource value: 0x7f050002
-			public const int studentID = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int studentID = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int studentIDInput = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int studentIDInput = 2131099651;
 			
-			// aapt resource value: 0x7f05000c
-			public const int testButton = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int testButton = 2131099660;
 			
 			static Id()
 			{
@@ -134,11 +157,11 @@ namespace ConnectUTS
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
