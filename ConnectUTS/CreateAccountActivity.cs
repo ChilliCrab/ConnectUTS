292098aa5473b344f36ff4c672eb6aac8e841799
kuramu1108 <mac@mbpr.local>
2015-10-15 09:28:22 +1100

    rename some variable

--- ConnectUTS/CreateAccountActivity.cs
+++ ConnectUTS/CreateAccountActivity.cs
@@ -32,7 +32,7 @@ namespace ConnectUTS
 			EditText nationalityInput = FindViewById<EditText> (Resource.Id.registerNationalityInput);
 			CheckBox agreeTos = FindViewById<CheckBox> (Resource.Id.registerAgreeTos);
 			Button registerAccountButton = FindViewById<Button> (Resource.Id.registerAccountButton);
-			Button cancelButton = FindViewById<Button> (Resource.Id.cancelButton);
+			Button cancelButton = FindViewById<Button> (Resource.Id.registerCancelButton);
 
 			// Set up fonts.
 			Typeface din = Typeface.CreateFromAsset (this.Assets, "fonts/din-regular.ttf");
